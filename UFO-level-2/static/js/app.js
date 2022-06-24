b203e38b24b6dca7f5e1466d24c4a5c46b175b3a
Nicholas McMahon (Corporate) <NMcMahon@nsrltd.com>
2022-06-24 09:15:47 +0800

    Final HW answers

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -9,7 +9,7 @@ console.log(tableData);
 
 // Use d3 to update each cell's text with UFO data values (date, City, State, Country, Shape, Duration, Comments)
 //Refactor to use Arrow Function
-    data.forEach((ufoData) => {
+    tableData.forEach((ufoData) => {
     var row = tbody.append("tr");
     Object.entries(ufoData).forEach(([key, value]) => {
     var cell = row.append("td");
@@ -26,29 +26,56 @@ button.on("click", runEnter);
 form.on("submit",runEnter);
 
 // Create an eventlistener to filter the table.
-function runEnter() {
-    //Clear out existing table
-    tbody.html("");
+function runEnter(){
     
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
+    
+
     // Select the user input location
-    var inputElement = d3.select('#datetime');
+    var inputDate = d3.select('#datetime');
+    var inputCity = d3.select('#city');
+    var inputState = d3.select('#state');
+    var inputCountry = d3.select('#country');
+    var inputShape = d3.select('#shape');
+
+
 
     // Get the value from the input
-    var inputValue = inputElement.property('value');
+    var inputValueDate = inputDate.property('value');
+    var inputValueCity = inputCity.property('value');
+    var inputValueState = inputState.property('value');
+    var inputValueCountry = inputCountry.property('value');
+    var inputValueShape = inputShape.property('value');
 
-    //console.log(inputvalue);
-
+    var filteredInput = data;
     // This code will filter the user input
-    var filteredInput = tableData.filter(data => data.datetime === inputValue);
-    
-    filteredInput.forEach(function(dateSelection){
+    if (inputValueDate) {
+        filteredInput = filteredInput.filter(selection => selection.datetime === inputValueDate);
+    };
+    if (inputValueCity) {
+        filteredInput = filteredInput.filter(selection => selection.city === inputValueCity);
+    };
+    if (inputValueState) {
+        filteredInput = filteredInput.filter(selection => selection.state === inputValueState);
+    };
+    if (inputValueCountry) {
+        filteredInput = filteredInput.filter(selection => selection.country === inputValueCountry);
+    };
+    if (inputValueShape) {
+        filteredInput = filteredInput.filter(selection => selection.shape === inputValueShape);
+    };
+
+    //Clear out existing table
+    tbody.html("");
+
+
+    filteredInput.forEach(function(selection){
         // for each element create a row
         var row = tbody.append("tr");
 
-        Object.entries(dateSelection).forEach(function([key, value]){
+        Object.entries(selection).forEach(function([key, value]){
             var cell = row.append("td");
             cell.text(value);
         });
